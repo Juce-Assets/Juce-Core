777ff1c926c5182a1ba26b8404bf44a739d78d8c
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-12-24 20:56:58 +0100

    Containers and pathfinding

---
+++ Containers/Int2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Juce.Core.Containers
+{
+    [System.Serializable]
+    public class Int2 : IEquatable<Int2>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Int2()
+        {
+
+        }
+
+        public Int2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Int2 other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+    }
+}
--- Pathfinding/IPathfindingAlgorithm.cs
+++ Pathfinding/IPathfindingAlgorithm.cs
@@ -2,7 +2,7 @@
 
 namespace Juce.Core.Pathfinding
 {
-    public interface IPathfindingAlgorithm<T>
+    public interface IPathfindingAlgorithm<T> where T : IEquatable<T>
     {
         bool Finished { get; }
         PathfindingPath<T> Result { get; }
