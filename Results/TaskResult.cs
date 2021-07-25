9662f3eae7350d83f7ad1027342f069df17fc466
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-07-25 22:23:41 +0200

    Result

---
+++ Results/TaskResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Juce.Core.Random
+{
+	public class TaskResult<T>
+	{
+		public bool HasResult { get; }
+		public T Value { get; }
+
+		private TaskResult(bool hasResult, T value)
+        {
+			HasResult = hasResult;
+			Value = value;
+		}
+
+		public static TaskResult<T> FromResult(T value)
+        {
+			return new TaskResult<T>(true, value);
+        }
+
+		public static TaskResult<T> FromError()
+		{
+			return new TaskResult<T>(false, default);
+		}
+	}
+}
