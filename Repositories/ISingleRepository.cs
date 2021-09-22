367d0ebfa26da8ba3720b30f0229053891f6da36
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-09-23 01:03:17 +0200

    Repos

---
+++ Repositories/ISingleRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Juce.Core.Repositories
+{
+    public interface ISingleRepository<TObject>
+    {
+        bool HasItem { get; }
+
+        void Set(TObject obj);
+        void Remove();
+        bool TryGet(out TObject obj);
+    }
+}
---
+++ Repositories/SimpleSingleRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Juce.Core.Repositories
+{
+    public class SimpleSingleRepository<TObject> : ISingleRepository<TObject>
+    {
+        private TObject item;
+
+        public bool HasItem { get; private set; }
+
+        public void Set(TObject obj)
+        {
+            item = obj;
+            HasItem = true;
+        }
+
+        public void Remove()
+        {
+            item = default;
+            HasItem = false;
+        }
+
+        public bool TryGet(out TObject obj)
+        {
+            obj = item;
+            return HasItem;
+        }
+    }
+}
