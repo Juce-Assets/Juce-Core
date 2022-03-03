2a559662ea9f3ec49640f4c569c8038ec9b16f67
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-03 20:10:53 +0100

    Refreshables

---
+++ Runtime/Refresh/CallbackRefreshable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Juce.Core.Refresh
+{
+    public class CallbackRefreshable : IRefreshable
+    {
+        private readonly Action onRefresh;
+
+        public CallbackRefreshable(Action onRefresh)
+        {
+            this.onRefresh = onRefresh;
+        }
+
+        public void Refresh()
+        {
+            onRefresh?.Invoke();
+        }
+    }
+}
---
+++ Runtime/Refresh/CompositeRefreshable.cs
@@ -0,0 +1,20 @@
+﻿namespace Juce.Core.Refresh
+{
+    public class CompositeRefreshable : IRefreshable
+    {
+        private readonly IRefreshable[] items;
+
+        public CompositeRefreshable(IRefreshable[] items)
+        {
+            this.items = items;
+        }
+
+        public void Refresh()
+        {
+            foreach(IRefreshable item in items)
+            {
+                item.Refresh();
+            }
+        }
+    }
+}
