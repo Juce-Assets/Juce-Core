c0fcc251fb115fef227988c03796f698448ca0a7
DESKTOP-OH1HMN0\guill <gsunyercaldu@gmail.com>
2022-03-19 20:27:45 +0100

    Callback

---
+++ Runtime/Tickable/CallbackTickable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Juce.Core.Tickable
+{
+    public class CallbackTickable : ITickable
+    {
+        private readonly Action callback;
+
+        public CallbackTickable(
+            Action callback
+            )
+        {
+            this.callback = callback;
+        }
+
+        public void Tick()
+        {
+            callback.Invoke();
+        }
+    }
+}
