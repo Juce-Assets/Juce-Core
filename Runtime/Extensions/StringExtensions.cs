5ba2c2f31cf593bc31f107df53794e7ce64f103a
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-06-02 22:50:01 +0200

    Extensions

---
+++ Runtime/Extensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Juce.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder(str);
+            stringBuilder[0] = char.ToUpper(stringBuilder[0]);
+
+            return stringBuilder.ToString();
+        }
+
+        public static Stream ToStream(this string str)
+        {
+            MemoryStream ret = new MemoryStream();
+
+            StreamWriter writer = new StreamWriter(ret);
+            writer.Write(str);
+            writer.Flush();
+
+            ret.Position = 0;
+
+            return ret;
+        }
+    }
+}
---
+++ Runtime/Extensions/TaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Juce.Core.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async void RunAsync(this Task task, Action onFinish = null)
+        {
+            await task;
+
+            onFinish?.Invoke();
+        }
+    }
+}
