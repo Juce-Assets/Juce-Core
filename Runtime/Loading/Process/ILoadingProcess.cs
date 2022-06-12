b2d488b26d4ffc2798100a8a38bf3a938eda95d4
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-06-12 21:59:12 +0200

    Loading

---
+++ Runtime/Loading/Process/ILoadingProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Juce.Core.Loading.Process
+{
+    public interface ILoadingProcess
+    {
+        event Action OnCompleted;
+
+        bool HasParent { get; }
+
+        ILoadingProcess NewChild();
+        void Complete();
+    }
+}
---
+++ Runtime/Loading/Process/LoadingProcess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Juce.Core.Loading.Process
+{
+    public sealed class LoadingProcess : ILoadingProcess
+    {
+        public event Action OnCompleted;
+
+        public bool HasParent { get; }
+
+        private LoadingProcess(bool hasParent)
+        {
+            HasParent = hasParent;
+        }
+
+        public static ILoadingProcess New()
+        {
+            return new LoadingProcess(hasParent: false);
+        }
+
+        public ILoadingProcess NewChild()
+        {
+            LoadingProcess childProcess = new LoadingProcess(hasParent: true);
+
+            return childProcess;
+        }
+
+        public void Complete()
+        {
+            OnCompleted?.Invoke();
+        }
+    }
+}
